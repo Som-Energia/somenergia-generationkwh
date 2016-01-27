9e5d16a8c2fb636dc0b7c960755a801fffd888d3
David García Garzón <voki@canvoki.net>
2016-01-27 15:40:13 +0100

    empty erp module

---
+++ generationkwh_api/__terp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+  "name": "Generation kWh",
+  "description": """Support for SomEnergia's Generation kWh in GisceERP""",
+  "version": "0.0.1",
+  "author": "GISCE",
+  "category": "Master",
+  "depends": ['base', 'som_polissa_soci'],
+  "init_xml": [],
+  "demo_xml": [],
+  "update_xml": [],
+  "active": False,
+  "installable": True
+}
