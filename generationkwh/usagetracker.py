9a6eefa097b563beb14e8c2fa66d91281ca735dd
David García Garzón <voki@canvoki.net>
2016-01-28 23:46:56 +0100

    refund implemented

--- generationkwh/usagetracker.py
+++ generationkwh/usagetracker.py
@@ -30,22 +30,19 @@ class UsageTracker(object):
         return allocated
 
     def refund_kwh(self, member, start, end, fare, period, kwh):
-        # Pseudo code spike, discard with real tests
-
-        usage = self._curves.usage(member, start, end)
+        production = self._curves.production(start, end) # member?
         periodMask = self._curves.periodMask(fare, period, start, end)
+        usage = self._curves.usage(member, start, end)
+
         deallocated = 0
-        for usebin, inperiod in zip(usage,faremask):
-            if not inperiod: continue # not in period
-            if usebin == 0 : continue # already unused
+        for i, (u, m) in enumerate(zip(usage, periodMask)):
+            if not m: continue # not in period
+            unused = min(kwh-deallocated, u)
+            usage[i] -= unused
+            deallocated += unused
 
-            deused = min(kwh-deallocated, usebin)
-            deallocated += deused
-            usebin -= deused # TODO in the array, i mean
-            if deallocated == kwh: break
-        return 69
+        return deallocated
 
-        self._curves.setUsage(member, start, end, faremask, usage)
 
 import unittest
 
@@ -213,6 +210,60 @@ class UsageTracker_Test(unittest.TestCase):
         real = t.use_kwh('soci', '2015-01-02', '2015-01-02', '2.0A', 'P1', 2)
         self.assertEqual(
              [1,2], curves.usage('soci', '2015-01-02', '2015-01-02'))
+        self.assertEqual(2, real)
+
+    def test_refund_singleBin(self):
+        curves=CurveProvider_MockUp(
+            production=[5,3],
+            usage=[3,0],
+            periodMask=[1,1],
+            )
+
+        t = UsageTracker(curves)
+        real = t.refund_kwh('soci', '2015-01-02', '2015-01-02', '2.0A', 'P1', 2)
+        self.assertEqual(
+             [1,0], curves.usage('soci', '2015-01-02', '2015-01-02'))
+        self.assertEqual(2, real)
+
+    def test_refund_severalBins_refundsForward(self):
+        # TODO: Is that the proper behaviour?
+        curves=CurveProvider_MockUp(
+            production=[5,3],
+            usage=[2,2],
+            periodMask=[1,1],
+            )
+
+        t = UsageTracker(curves)
+        real = t.refund_kwh('soci', '2015-01-02', '2015-01-02', '2.0A', 'P1', 3)
+        self.assertEqual(
+             [0,1], curves.usage('soci', '2015-01-02', '2015-01-02'))
+        self.assertEqual(3, real)
+
+    def test_refund_beyondUsed(self):
+        # TODO: Is that the proper behaviour?
+        curves=CurveProvider_MockUp(
+            production=[5,3],
+            usage=[2,2],
+            periodMask=[1,1],
+            )
+
+        t = UsageTracker(curves)
+        real = t.refund_kwh('soci', '2015-01-02', '2015-01-02', '2.0A', 'P1', 5)
+        self.assertEqual(
+             [0,0], curves.usage('soci', '2015-01-02', '2015-01-02'))
+        self.assertEqual(4, real)
+
+    def test_refund_masked(self):
+        curves=CurveProvider_MockUp(
+            production=[5,3],
+            usage=[2,2],
+            periodMask=[0,1],
+            )
+
+        t = UsageTracker(curves)
+        real = t.refund_kwh('soci', '2015-01-02', '2015-01-02', '2.0A', 'P1', 2)
+        self.assertEqual(
+             [2,0], curves.usage('soci', '2015-01-02', '2015-01-02'))
         self.assertEqual(2, real)
 
 
