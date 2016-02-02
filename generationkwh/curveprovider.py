ecd1857db0701450d334822079cc0965d21421a2
David García Garzón <voki@canvoki.net>
2016-02-02 19:17:15 +0100

    interface for a curve provider

---
+++ generationkwh/curveprovider.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+class CurveProvider(object):
+	""" Provides hourly curves required to track
+		generationkwh member usage.
+	"""
+
+	def production(self, member, start, end):
+		""" Returns acquainted productions rights for
+			the member
+			within start and end dates, included.
+		"""
+
+	def usage(self, member, start, end):
+		""" Return used production rights for the member
+			within start and end dates, included.
+		"""
+
+	def periodMask(self, fare, period, start, end):
+		""" Returns an array for the set of hours
+			the period is active por a given fare
+			within start and end days, included.
+		"""
+
+class CurveProvider_Test(unittest.TestCase):
+
+	def test_me(self):
+		self.fail("It works!")
+
+
+
+
