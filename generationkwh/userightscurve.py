fabae1c3109ee83cfe4c5102bbb7ad77faaa311e
David García Garzón <voki@canvoki.net>
2016-02-16 22:35:01 +0100

    userightscurve: eager version

--- generationkwh/userightscurve.py
+++ generationkwh/userightscurve.py
@@ -1,11 +1,32 @@
 # -*- coding: utf-8 -*-
 
+import numpy
+
 class UseRightsCurve(object):
     """
+        Provides the hourly curve of kWh available for a given
+        member in a given time interval.
+
+        Naive and eager versions are provided:
+
+        - Naive version just multiplies the kWh discretized curve
+        for one action by the number of active shares.
+
+        - Eager version takes a different profile for each number
+        of shares and members with N shares get a kWh with N times
+        earlier.
+
+        kWh remainder are accommulated to the next hour so if the
+        next hour implies a change of period, that energy is wrongly
+        assigned, to be purist.
+
+        That error is bigger for the naive approximation, as the
+        error in every period change is up to N kWh if you have N shares.
+        The eager version limits the error a 1 kWh.
     """
-    def __init__(self, activeShares, rightsPerAction, eager=False):
+    def __init__(self, activeShares, rightsPerShare, eager=False):
         self._activeShares = activeShares
-        self._rightsPerAction = rightsPerAction
+        self._rightsPerAction = rightsPerShare
         self._eager = eager
 
     def get(self, member, start, end):
@@ -16,12 +37,20 @@ class UseRightsCurve(object):
                     nshares, start, end)) *
                 numpy.array(self._activeShares.hourly(member, start, end))
                 )
+
+        shares = self._activeShares.hourly(member, start, end)
+        choiceset = list(sorted(set(shares)))
+        choices = [
+            self._rightsPerAction.rightsPerShare(nshares, start, end)
+            if nshares in choiceset
+            else None
+            for nshares in xrange(max(choiceset)+1)
+            ]
+        return numpy.choose(shares, choices)
  
 
 import unittest
-from activesharescurve import ActiveSharesCurve, UnconfiguredDataProvider
 import datetime
-import numpy
 
 def isodate(date):
     return datetime.datetime.strptime(date, '%Y-%m-%d').date()
@@ -36,26 +65,26 @@ class CurveProvider_MockUp(object):
 
 class UseRightsCurve_Test(unittest.TestCase):
     def asserUseRightsEqual(self, member, start, end,
-            activeShares, rightsPerAction, expected
+            activeShares, rightsPerShare, expected, eager=False
             ):
         curve = UseRightsCurve(
             activeShares=CurveProvider_MockUp(activeShares),
-            rightsPerAction=CurveProvider_MockUp(rightsPerAction),
+            rightsPerShare=CurveProvider_MockUp(rightsPerShare),
+            eager = eager,
             )
         result = curve.get(member,
             start=isodate(start),
             end=isodate(end),
             )
         self.assertEqual(list(result), expected)
-            
 
-    def test_(self):
+    def test_singleShare(self):
         self.asserUseRightsEqual(
             member='member',
             start='2015-01-21',
             end='2015-01-21',
             activeShares = 25*[1],
-            rightsPerAction = {1: 25*[3]},
+            rightsPerShare = {1: 25*[3]},
             expected = 25*[3]
             )
 
@@ -65,7 +94,7 @@ class UseRightsCurve_Test(unittest.TestCase):
             start='2015-01-21',
             end='2015-01-21',
             activeShares = 25*[2],
-            rightsPerAction = {1: 25*[3]},
+            rightsPerShare = {1: 25*[3]},
             expected = 25*[6]
             )
 
@@ -75,23 +104,36 @@ class UseRightsCurve_Test(unittest.TestCase):
             start='2015-01-21',
             end='2015-01-22',
             activeShares = 25*[2] + 25*[3],
-            rightsPerAction = {1: 50*[3]},
+            rightsPerShare = {1: 50*[3]},
             expected = 25*[6] + 25*[9],
             )
 
-    def assertEagerRightsEqual(self, member, start, end,
-            activeShares, rightsPerAction, expected
-            ):
-        curve = UseRightsCurve(
-            activeShares=CurveProvider_MockUp(activeShares),
-            rightsPerAction=CurveProvider_MockUp(rightsPerAction),
+    def test_eager_uniformShares(self):
+        self.asserUseRightsEqual(
+            member='member',
+            start='2015-01-21',
+            end='2015-01-22',
+            activeShares = 25*[2],
+            rightsPerShare = {
+                2: 25*[20],
+                },
+            expected = 25*[20],
+            eager = True
             )
-        result = curve.get(member,
-            start=isodate(start),
-            end=isodate(end),
-            )
-        self.assertEqual(list(result), expected)
 
+    def test_eager_mixedShares(self):
+        self.asserUseRightsEqual(
+            member='member',
+            start='2015-01-21',
+            end='2015-01-22',
+            activeShares = 25*[2] + 25*[3],
+            rightsPerShare = {
+                2: 50*[20],
+                3: 50*[70],
+                },
+            expected = 25*[20] + 25*[70],
+            eager = True
+            )
 
 
 # vim: ts=4 sw=4 et
