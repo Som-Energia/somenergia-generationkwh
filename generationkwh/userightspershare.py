bbc27aa72e925399bc7065aef567adac986bc4ca
David García Garzón <voki@canvoki.net>
2016-02-17 20:17:06 +0100

    remainder management

--- generationkwh/userightspershare.py
+++ generationkwh/userightspershare.py
@@ -2,8 +2,7 @@
 
 # TODO:
 # - total active shares in a day = 0
-# - consider successive remainders
-# - caching values
+# - non-adjacent cache and production
 
 import numpy
 
@@ -18,13 +17,23 @@ class UserRightsPerShare(object):
         self._cache = cache
 
     def get(self, nshares, start, end):
-        hoursADay = 25
         activeShares = self._activeShares.hourly(None, start, end)
-        production = self._production.get(start, end)
+        assert activeShares.dtype == 'int64', "ActiveShares base type is not integer"
 
-        fraction = production*nshares/activeShares
-        fraction = numpy.concatenate( ([0],fraction) )
-        return numpy.diff(fraction.cumsum()//1000)
+        production = self._production.get(start, end)
+        assert production.dtype == 'int64', "Production base type is not integer"
+
+        cacheDate, cacheRemainder = self._cache.lastRemainder(nshares) if self._cache else (None, 0)
+
+        with numpy.errstate(divide='ignore'):
+            fraction = production*nshares/activeShares
+        fraction = numpy.concatenate( ([cacheRemainder],fraction) )
+        integral = fraction.cumsum()
+
+        result = numpy.diff(integral//1000)
+        if self._cache is not None:
+            self._cache.update(nshares, result, end, integral[-1]%1000)
+        return result
 
 
 import unittest
@@ -49,8 +58,13 @@ class Cache_Mockup(object):
     def lastRemainder(self, nShares):
         return self._date, self._remainder
 
-    def get(self, nshares, start, end):
+    def get(self, nShares, start=None, end=None):
         return self._curve
+
+    def update(self, nShares, curve, end, remainder):
+        self._curve = numpy.concatenate((self._curve, curve))
+        self._date = end
+        self._remainder = remainder
 
 
 def isodate(date):
@@ -129,6 +143,16 @@ class UserRightsPerShare_Test(unittest.TestCase):
             expected = 12*[0,1]+[0],
             )
 
+    def test_get_withNoShare(self):
+        self.assertUserRightsPerShareEquals(
+            production = 25*[1000],
+            activeShares = 10*[0] + 15*[1],
+            nShares=1,
+            start='2015-01-02',
+            end='2015-01-02',
+            expected = 10*[0]+15*[1],
+            )
+
 
     def assertCachedResults(self,
         cache,
@@ -144,7 +168,8 @@ class UserRightsPerShare_Test(unittest.TestCase):
         expectedCacheDate,
         expectedCacheRemainder,
         ):
-        cache = Cache_Mockup(cache, cacheDate, cacheRemainder)
+
+        cache = Cache_Mockup(numpy.array(cache), cacheDate, cacheRemainder)
 
         curve = UserRightsPerShare(
             production = Curve_MockUp(production),
@@ -157,8 +182,12 @@ class UserRightsPerShare_Test(unittest.TestCase):
             end=isodate(end),
             )
         self.assertEqual(list(result), expected)
+        self.assertEqual(list(cache.get(nShares)), expectedCache)
+        self.assertEqual(cache.lastRemainder(nShares),
+            (isodate(expectedCacheDate), expectedCacheRemainder))
 
-    def test_cache(self):
+
+    def test_cache_with_nocache(self):
         self.assertCachedResults(
             cache = [],
             cacheDate = None,
@@ -168,12 +197,75 @@ class UserRightsPerShare_Test(unittest.TestCase):
             nShares=1,
             start='2015-01-02',
             end='2015-01-02',
-            expected = 12*[0,1]+[0],
-            expectedCache = 12*[0,1]+[0],
+            expected = 25*[1],
+            expectedCache = 25*[1],
             expectedCacheDate = '2015-01-02',
             expectedCacheRemainder = 0,
             )
 
+    def test_cache_creatingRemainder(self):
+        self.assertCachedResults(
+            cache = [],
+            cacheDate = None,
+            cacheRemainder = 0.0,
+            production = 25*[500],
+            activeShares = 25*[1],
+            nShares=1,
+            start='2015-01-02',
+            end='2015-01-02',
+            expected = 12*[0,1]+[0],
+            expectedCache = 12*[0,1]+[0],
+            expectedCacheDate = '2015-01-02',
+            expectedCacheRemainder = 500,
+            )
+
+    def test_cache_takingRemainder(self):
+        self.assertCachedResults(
+            cache = [],
+            cacheDate = '2015-01-01',
+            cacheRemainder = 500.0,
+            production = 25*[500],
+            activeShares = 25*[1],
+            nShares=1,
+            start='2015-01-02',
+            end='2015-01-02',
+            expected = 12*[1,0]+[1],
+            expectedCache = 12*[1,0]+[1],
+            expectedCacheDate = '2015-01-02',
+            expectedCacheRemainder = 0,
+            )
+
+    def test_get_floatProductionFailsAssertion(self):
+        production = [3.2]
+        activeShares = [1]
+        curve = UserRightsPerShare(
+            production = Curve_MockUp(production),
+            activeShares = Curve_MockUp(activeShares),
+            )
+        with self.assertRaises(AssertionError) as failure:
+            curve.get(
+                nshares=1,
+                start=isodate('2015-01-02'),
+                end=isodate('2015-01-02'),
+                )
+        self.assertEqual(failure.exception.args[0],
+            "Production base type is not integer" )
+
+    def test_get_floatSharesFailsAssertion(self):
+        production = [3]
+        activeShares = [3.2]
+        curve = UserRightsPerShare(
+            production = Curve_MockUp(production),
+            activeShares = Curve_MockUp(activeShares),
+            )
+        with self.assertRaises(AssertionError) as failure:
+            curve.get(
+                nshares=1,
+                start=isodate('2015-01-02'),
+                end=isodate('2015-01-02'),
+                )
+        self.assertEqual(failure.exception.args[0],
+            "ActiveShares base type is not integer" )
 
 
 
