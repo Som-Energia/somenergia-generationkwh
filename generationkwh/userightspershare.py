6c5a260b151292262f04e0937c761b7fefb119de
David García Garzón <voki@canvoki.net>
2016-02-18 13:13:06 +0100

    extracted computeRights

--- generationkwh/userightspershare.py
+++ generationkwh/userightspershare.py
@@ -16,6 +16,18 @@ class UserRightsPerShare(object):
         self._activeShares = activeShares
         self._cache = cache
 
+    def computeRights(self, previousRemainder, production, nshares, activeShares):
+
+        with numpy.errstate(divide='ignore'):
+            fraction = production*nshares/activeShares
+        fraction = numpy.concatenate( ([previousRemainder],fraction) )
+        integral = fraction.cumsum()
+
+        result = numpy.diff(integral//1000)
+        remainder = integral[-1]%1000
+
+        return result, remainder
+
     def get(self, nshares, start, end):
         cacheDate, cacheRemainder = self._cache.lastRemainder(nshares) if self._cache else (None, 0)
 
@@ -76,6 +88,95 @@ def isodate(date):
 
 
 class UserRightsPerShare_Test(unittest.TestCase):
+
+    def assertComputeEquals(self,
+        production,
+        activeShares,
+        nShares,
+        expected,
+        expectedRemainder=0,
+        remainder = 0,
+        ):
+
+        curve = UserRightsPerShare(
+            production = Curve_MockUp(production),
+            activeShares = Curve_MockUp(activeShares),
+            )
+        result, resultingRemainder = curve.computeRights(
+            remainder,
+            production=numpy.array(production),
+            nshares=nShares,
+            activeShares = numpy.array(activeShares),
+            )
+        self.assertEqual(list(result), expected)
+        self.assertEqual(resultingRemainder, expectedRemainder)
+
+    def test_computeRights_noProduction(self):
+        self.assertComputeEquals(
+            production = 25*[0],
+            activeShares = 25*[1],
+            nShares=1,
+            expected = 25*[0],
+            )
+
+    def test_computeRights_uniformProduction(self):
+        self.assertComputeEquals(
+            production = 25*[2000],
+            activeShares = 25*[1],
+            nShares=1,
+            expected = 25*[2],
+            )
+
+    def test_computeRights_sharedProduction(self):
+        self.assertComputeEquals(
+            production = 25*[2000],
+            activeShares = 25*[2],
+            nShares=1,
+            expected = 25*[1],
+            )
+
+    def test_computeRights_withManyShares(self):
+        self.assertComputeEquals(
+            production = 25*[2000],
+            activeShares = 25*[2],
+            nShares=2,
+            expected = 25*[2],
+            )
+
+    def test_computeRights_withInnerRemainder(self):
+        self.assertComputeEquals(
+            production = 24*[1000]+[0],
+            activeShares = 25*[2],
+            nShares=1,
+            expected = 12*[0,1]+[0],
+            )
+
+    def test_computeRights_withNoShare(self):
+        self.assertComputeEquals(
+            production = 25*[1000],
+            activeShares = 10*[0] + 15*[1],
+            nShares=1,
+            expected = 10*[0]+15*[1],
+            )
+
+    def test_computeRights_creatingRemainder(self):
+        self.assertComputeEquals(
+            production = 25*[500],
+            activeShares = 25*[1],
+            nShares=1,
+            expected = 12*[0,1]+[0],
+            expectedRemainder = 500,
+            )
+
+    def test_computeRights_takingRemainder(self):
+        self.assertComputeEquals(
+            remainder = 500.0,
+            production = 25*[500],
+            activeShares = 25*[1],
+            nShares=1,
+            expected = 12*[1,0]+[1],
+            expectedRemainder = 0,
+            )
 
     def test_get_floatProductionFailsAssertion(self):
         production = [3.2]
