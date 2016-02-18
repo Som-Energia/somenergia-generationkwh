19729f0159d644b9870af77c5cc4384718cbf309
abbse <aleix.badia@somenergia.coop>
2016-02-18 20:06:38 +0100

    configurable step in daterange

--- plantmeter/utils.py
+++ plantmeter/utils.py
@@ -1,7 +1,8 @@
 from datetime import timedelta, date
 
-def daterange(start_date, end_date):
+def daterange(start_date, end_date, **kwds):
+    step = kwds or dict(days=1)
     if not end_date:
         end_date = start_date + timedelta(1)
     for n in range(int ((end_date - start_date).days)):
-        yield start_date + timedelta(n)
+        yield start_date + n*timedelta(**step)
