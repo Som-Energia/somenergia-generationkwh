e7b1bc29cccf6ec1ae2f8bc1057732a8f43a3efb
David García Garzón <voki@canvoki.net>
2016-02-23 17:32:00 +0100

    delegating Dealer creation to a factory method

--- generationkwh_api/generationkwh_api.py
+++ generationkwh_api/generationkwh_api.py
@@ -12,12 +12,18 @@ class GenerationkWhDealer(osv.osv):
     _name = 'generationkwh.dealer'
     _auto = False
 
+
+    def _createDealer(self, cursor, uid):
+        # TODO: Feed the dealer with data sources
+        return Dealer()
+
+
     def is_active(self, cursor, uid,
                   contract_id, start_date, end_date,
                   context=None):
         """ Returns True if contract_id has generation kwh activated
             during the period"""
-        dealer = Dealer()
+        dealer = self._createDealer(cursor, uid)
 
         return dealer.is_active(
             contract_id, start_date, end_date)
@@ -27,18 +33,18 @@ class GenerationkWhDealer(osv.osv):
                           context=None):
         """ Returns generationkwh [kWh] available for contract_id during the
             date interval, fare and period"""
-        dealer = Dealer()
+        dealer = self._createDealer(cursor, uid)
 
         return dealer.get_available_kwh(
             contract_id, start_date, end_date, fare, period)
 
     def use_kwh(self, cursor, uid,
-                contract_id, start_date, end_date, fare, period, kwh,
+                contract_ia, start_date, end_date, fare, period, kwh,
                 context=None):
         """Marks the indicated kwh as used, if available, for the contract,
            date interval, fare and period and returns the ones efectively used.
         """
-        dealer = Dealer()
+        dealer = self._createDealer(cursor, uid)
         return dealer.use_kwh(
             contract_id, start_date, end_date, fare, period, kwh)
 
@@ -49,10 +55,13 @@ class GenerationkWhDealer(osv.osv):
            contract, date interval, fare and period and returns the ones
            efectively used.
         """
-        dealer = Dealer()
+        dealer = self._createDealer(cursor, uid)
+
         return dealer.refund_kwh(
             contract_id, start_date, end_date, fare, period, kwh)
 
 GenerationkWhDealer()
+
+
 
 # vim: ts=4 sw=4 et
