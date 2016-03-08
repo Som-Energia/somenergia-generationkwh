02d5f139ed5e984d9b5dfbe4f61311be8bef753e
David García Garzón <voki@canvoki.net>
2016-03-08 18:04:23 +0100

    mongotimecurve: working but it's all fake

--- plantmeter/mongotimecurve.py
+++ plantmeter/mongotimecurve.py
@@ -5,15 +5,28 @@ import unittest
 from pymongo import Connection
 from yamlns import namespace as ns
 import json
+import numpy
+import datetime
 
 class MongoTimeCurve(object):
     """Consolidates curve data in a mongo database"""
 
     def __init__(self, uri, collection):
         ""
+        self.data=numpy.array([0]*25)
 
     def get(self, start, stop, filter, field):
-        return [0]*25
+        return self.data
+
+    def fillPoint(self, data):
+        self.data[-2]=10
+        
+
+def isodatetime(string):
+    return datetime.datetime.strptime(string, "%Y-%m-%d %H:%M:%S")
+
+def isodate(string):
+    return datetime.datetime.strptime(string, "%Y-%m-%d").date()
 
 class MongoTimeCurve_Test(unittest.TestCase):
 
@@ -32,17 +45,34 @@ class MongoTimeCurve_Test(unittest.TestCase):
 
 
     def test_get_whenEmpty(self):
-        
         mtc = MongoTimeCurve(self.dburi, self.collection)
         curve = mtc.get(
-            start='2015-01-01',
-            stop='2015-01-01',
+            start=isodate('2015-01-01'),
+            stop=isodate('2015-01-01'),
             filter='miplanta',
             field='ae',
             )
         self.assertEqual(
             list(curve),
             [0]*25)
+
+    def test_get_withSingleData(self):
+        mtc = MongoTimeCurve(self.dburi, self.collection)
+        mtc.fillPoint(ns(
+            datetime=isodatetime('2015-01-01 23:00:00'),
+            name='miplanta',
+            ae=10,
+            ))
+
+        curve = mtc.get(
+            start=isodate('2015-01-01'),
+            stop=isodate('2015-01-01'),
+            filter='miplanta',
+            field='ae',
+            )
+        self.assertEqual(
+            list(curve),
+            [0]*23+[10,0])
 
 
             
