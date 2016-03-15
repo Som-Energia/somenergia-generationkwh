ced14957c02813ed0480d1bd916e8552bce088bb
Agustí Fita i Borrell <afita@gisce.net>
2016-03-15 15:54:07 +0100

    IMP Development mockup update

--- generationkwh/dealer.py
+++ generationkwh/dealer.py
@@ -9,8 +9,11 @@ class Dealer(object):
             contract_id, start_date, end_date):
         """ Returns True if contract_id has generation kwh activated
             during the period"""
+        if contract_id == 4:
+            return True
         return False
 
+    # Do not use for invoicing, ONLY statistics
     def get_available_kwh(self, contract_id, start_date, end_date, fare, period):
         """ Returns generationkwh [kWh] available for contract_id during the
             date interval, fare and period"""
@@ -20,9 +23,13 @@ class Dealer(object):
         """Marks the indicated kwh as used, if available, for the contract,
            date interval, fare and period and returns the ones efectively used.
         """
-        return kwh
+        return [
+            {'member_id': 13, 'kwh': int(round(kwh / 2))},
+            {'member_id': 42, 'kwh': int(round(kwh / 4))}
+        ]
 
-    def refund_kwh(self, contract_id, start_date, end_date, fare, period, kwh):
+    def refund_kwh(self, contract_id, start_date, end_date, fare, period, kwh,
+                   partner_id):
         """Refunds the indicated kwh, marking them as available again, for the
            contract, date interval, fare and period and returns the ones
            efectively used.
