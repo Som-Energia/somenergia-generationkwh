1a9c79d9e01689f836387a737c2172e75394cfc4
David García Garzón <voki@canvoki.net>
2016-04-14 17:47:41 +0200

    indentation

--- scripts/assignment_test.py
+++ scripts/assignment_test.py
@@ -41,7 +41,11 @@ class Assignment_Test(unittest.TestCase):
                             for r in result], 
                          expectation)
     def tearDown(self):
-        for a in self.Assignments.browse(['|', ('active', '=', False), ('active', '=', True)]):
+        for a in self.Assignments.browse([
+            '|',
+            ('active', '=', False),
+            ('active', '=', True),
+            ]):
             a.unlink()
     def test_no_assignments(self):
         self.setupProvider()
@@ -50,38 +54,72 @@ class Assignment_Test(unittest.TestCase):
     def test_one_assignment(self):
         rp=self.erp.ResPartner.browse([],limit=1)[0]
         gp=self.erp.GiscedataPolissa.browse([], limit=1)[0]
-        self.setupProvider([[True,gp.id,rp.id,1]])
-        self.assertAssignmentsEqual([[True,gp.id,rp.id,1]])
+        self.setupProvider([
+            [True,gp.id,rp.id,1],
+            ])
+        self.assertAssignmentsEqual([
+            [True,gp.id,rp.id,1]
+            ])
 
     def test_no_duplication(self):
         rp=self.erp.ResPartner.browse([], limit=1)[0]
         gp=self.erp.GiscedataPolissa.browse([],limit=1)[0]
-        self.setupProvider([[True,gp.id,rp.id,1],[True,gp.id,rp.id,1]])
-        self.assertAllAssignmentsEqual([[False, gp.id, rp.id, 1], [True,gp.id,rp.id,1]])
+        self.setupProvider([
+            [True, gp.id, rp.id, 1],
+            [True, gp.id, rp.id, 1],
+            ])
+        self.assertAllAssignmentsEqual([
+            [False, gp.id, rp.id, 1],
+            [True, gp.id, rp.id, 1],
+            ])
     
     def test_change_priority(self):
         rp=self.erp.ResPartner.browse([], limit=1)[0]
         gp=self.erp.GiscedataPolissa.browse([],limit=1)[0]
-        self.setupProvider([[True,gp.id,rp.id,1],[True,gp.id,rp.id,2]])
-        self.assertAllAssignmentsEqual([[False, gp.id, rp.id, 1], [True,gp.id,rp.id,2]])
+        self.setupProvider([
+            [True,gp.id,rp.id,1],
+            [True,gp.id,rp.id,2],
+            ])
+        self.assertAllAssignmentsEqual([
+            [False, gp.id, rp.id, 1],
+            [True,gp.id,rp.id,2]
+            ])
         
     def test_three_member_three_polissas(self):
         rp=self.erp.ResPartner.browse([],limit=3)
         gp=self.erp.GiscedataPolissa.browse([], limit=3)
-        self.setupProvider([[True,gp_iter.id,rp_iter.id,1] for gp_iter,rp_iter in zip(gp,rp)])
-        self.assertAssignmentsEqual([[True,gp_iter.id,rp_iter.id,1] for gp_iter,rp_iter in zip(gp,rp)])
+        self.setupProvider([
+            [True,gp_iter.id,rp_iter.id,1]
+            for gp_iter,rp_iter in zip(gp,rp)
+            ])
+        self.assertAssignmentsEqual([
+            [True,gp_iter.id,rp_iter.id,1]
+            for gp_iter,rp_iter in zip(gp,rp)
+            ])
 
     def test_three_member_one_polissa(self):
         rp=self.erp.ResPartner.browse([],limit=3)
         gp=self.erp.GiscedataPolissa.browse([], limit=1)[0]
-        self.setupProvider([[True,gp.id,rp_iter.id,1] for rp_iter in rp])
-        self.assertAssignmentsEqual([[True,gp.id,rp_iter.id,1] for rp_iter in rp])
+        self.setupProvider([
+            [True,gp.id,rp_iter.id,1]
+            for rp_iter in rp
+            ])
+        self.assertAssignmentsEqual([
+            [True,gp.id,rp_iter.id,1]
+            for rp_iter in rp
+            ])
 
     def test_one_member_three_polissas(self):
         rp=self.erp.ResPartner.browse([],limit=1)[0]
         gp=self.erp.GiscedataPolissa.browse([], limit=3)
-        self.setupProvider([[True,gp_iter.id,rp.id,1] for gp_iter in gp])
-        self.assertAssignmentsEqual([[True,gp_iter.id,rp.id,1] for gp_iter in gp])
+        self.setupProvider([
+            [True,gp_iter.id,rp.id,1]
+            for gp_iter in gp
+            ])
+        self.assertAssignmentsEqual([
+            [True,gp_iter.id,rp.id,1]
+            for gp_iter in gp
+            ])
 
 if __name__ == '__main__':
     unittest.main()
