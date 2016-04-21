58e0fbd55eb824aa0085008cc71d54a4b1e651b5
David García Garzón <voki@canvoki.net>
2016-04-21 18:56:43 +0200

    extracted productionloader from generationkwh_api

--- generationkwh_api/generationkwh_api.py
+++ generationkwh_api/generationkwh_api.py
@@ -7,53 +7,18 @@ import netsvc
 from mongodb_backend.mongodb2 import mdbpool
 
 from generationkwh.dealer import Dealer
-from generationkwh.sharescurve import MemberSharesCurve, PlantSharesCurve
+from generationkwh.sharescurve import MemberSharesCurve
 from generationkwh.rightspershare import RightsPerShare
 from generationkwh.memberrightscurve import MemberRightsCurve
 from generationkwh.memberrightsusage import MemberRightsUsage
 from generationkwh.fareperiodcurve import FarePeriodCurve
 from generationkwh.usagetracker import UsageTracker
-from generationkwh.productionloader import ProductionLoader
 from generationkwh.isodates import localisodate
 from .assignment import AssignmentProvider
 from .remainder import RemainderProvider
 from .investment import InvestmentProvider
 from .holidays import HolidaysProvider
-
-# Data providers
-
-from .erpwrapper import ErpWrapper
-
-class ProductionAggregatorProvider(ErpWrapper):
-    def __init__(self, erp, cursor, uid, pid, context=None):
-        self.pid = pid
-        super(ProductionAggregatorProvider, self).__init__(erp, cursor, uid, context)
-
-    def getWh(self, start, end):
-        production=self.erp.pool.get('generationkwh.production.aggregator')
-        return production.getWh(self.cursor, self.uid,
-                self.pid, start, end, context=self.context)
-
-    def updateWh(self, start, end):
-        production=self.erp.pool.get('generationkwh.production.aggregator')
-        return production.updateWh(self.cursor, self.uid,
-                self.pid, start, end, context=self.context)
-
-    def getFirstMeasurementDate(self):
-        production=self.erp.pool.get('generationkwh.production.aggregator')
-        return production.firstMeasurementDate(self.cursor, self.uid,
-                self.pid, context=self.context)
-
-    def getLastMeasurementDate(self):
-        production=self.erp.pool.get('generationkwh.production.aggregator')
-        return production.lastMeasurementDate(self.cursor, self.uid,
-                self.pid, context=self.context)
-
-    def getNshares(self, pid):
-        production=self.erp.pool.get('generationkwh.production.aggregator')
-        return production.getNshares(self.cursor, self.uid,
-                pid, context=self.context)
-
+from .productionloader import ProductionAggregatorProvider # unused, force load
 
 # Models
 
@@ -151,42 +116,6 @@ class GenerationkWhTestHelper(osv.osv):
 GenerationkWhTestHelper()
 
 
-
-class GenerationkWhProductionLoader(osv.osv):
-
-    _name = 'generationkwh.production.loader'
-    _auto = False
-
-    def _createProductionLoader(self, cursor, uid, pid, context):
-        production = ProductionAggregatorProvider(self, cursor, uid, pid, context)
-        nshares = production.getNshares(pid)
-        shares = PlantSharesCurve(nshares)
-        rights = RightsPerShare(mdbpool.get_db())
-        remainders = RemainderProvider(self, cursor, uid, context)
-
-        return ProductionLoader(
-                productionAggregator=production,
-                plantShareCurver=shares,
-                rightsPerShare=rights,
-                remainders=remainders)
-
-    def computeAvailableRights(self, cursor, uid, pid, context=None):
-        logger = netsvc.Logger()
-        productionLoader = self._createProductionLoader(cursor, uid, pid, context)
-        productionLoader.computeAvailableRights()
-
-        logger.notifyChannel('gkwh_productionLoader COMPUTE', netsvc.LOG_INFO,
-                'Compute available rights')
-
-    def retrieveMeasuresFromPlants(self, cursor, uid, pid, start, end, context=None):
-        logger = netsvc.Logger()
-        productionLoader = self._createProductionLoader(cursor, uid, pid, context)
-        productionLoader.retrieveMeasuresFromPlants(localisodate(start), localisodate(end))
-
-        logger.notifyChannel('gkwh_productionLoader UPDATE', netsvc.LOG_INFO,
-                'Retrieve measurements from plant')
-
-GenerationkWhProductionLoader()
 
 
 class GenerationkWhDealer(osv.osv):
