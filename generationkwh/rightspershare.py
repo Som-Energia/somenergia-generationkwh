800e9d4d035e1a4af4ebc7cc39ca0a64b7ed03fb
David García Garzón <david.garcia@upf.edu>
2016-04-26 07:52:51 +0200

    rightspershare: documenting methods

--- generationkwh/rightspershare.py
+++ generationkwh/rightspershare.py
@@ -13,13 +13,27 @@ class RightsPerShare(object):
             self.db,'rightspershare')
 
     def rightsPerShare(self, nshares, start, stop):
+        """
+            Returns the curve of hourly generated rights in kwh
+            for members with `nshares` active shares,
+            between start and stop dates both included.
+        """
+        # TODO: No remainder for nshares? -> create 0Wh remainder and use 1 shares and multiply by n
+
         return self.curve.get(
             filter=str(nshares),
             start=start,
             stop=stop,
             field='rights_kwh',
             )
+
     def updateRightsPerShare(self, nshares, start, data):
+        """
+            Sets the curve of hourly consolidated rights in kwh
+            for members with nshares active shares,
+            between start and stop dates both included.
+        """
+
         curve = self.curve.update(
             start=start,
             filter=str(nshares),
