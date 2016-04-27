e62152933af83a948c9501e9810dfde0984b5b27
César López Ramírez <cesar.lopez@somenergia.coop>
2016-04-27 16:30:10 +0200

    Payer first in assignments default sql

--- sql/asignacion_total_b2b.sql
+++ sql/asignacion_total_b2b.sql
@@ -2,8 +2,8 @@
 	pol.name as polissa_id,
         soci.id AS soci_id,
         CASE
-            WHEN pol.pagador=soci.id THEN 2
-            WHEN pol.titular=soci.id THEN 1
+            WHEN pol.pagador=soci.id THEN 1
+            WHEN pol.titular=soci.id THEN 2
             END AS PRIORIDAD
     FROM res_partner AS soci
     LEFT JOIN
@@ -13,17 +13,6 @@
     LEFT JOIN
         giscedata_cups_ps AS cups ON
             cups.id = pol.cups
-    INNER JOIN (
-        SELECT 
-            sum(LINE.AMOUNT) AS inversion,
-            max(partner_id) AS already_invested
-        FROM payment_line AS line
-        LEFT JOIN
-            payment_order AS remesa ON remesa.id = line.order_id 
-        WHERE
-            remesa.mode = 19
-        GROUP BY partner_id
-        ) AS investments on already_invested=soci.id
     WHERE
         soci.active AND
         pol.state = 'activa' AND
