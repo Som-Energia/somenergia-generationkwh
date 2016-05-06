f333c1ca0b8feb25c709db524349f9eeebe17789
David García Garzón <voki@canvoki.net>
2016-05-06 15:03:08 +0200

    command lining

--- scripts/genkwh_assignments.py
+++ scripts/genkwh_assignments.py
@@ -22,7 +22,7 @@ def parseArgumments():
     list = subparsers.add_parser('list',
         help="list assignments",
         )
-    set = subparsers.add_parser('set',
+    assign = subparsers.add_parser('assign',
         help="set assignment",
         )
     expire = subparsers.add_parser('expire',
@@ -35,29 +35,14 @@ def parseArgumments():
         help="clear investments objects",
         )
 
-    for sub in set,:
-        sub.add_argument('contract')
-        sub.add_argument('member')
-        sub.add_argument('priority')
-
-    for sub in expire,:
-        sub.add_argument(
-            'contract',
-            type=int,
-            )
-        sub.add_argument(
-            'member',
-            type=int,
-            )
-    for sub in default,:
-        
+    for sub in default, expire, assign:
         sub.add_argument(
             '-p','--partner',
             dest='idmode',
             action='store_const',
             const='partner',
             help="select members by its partner database id, "
-                "instead member database id",
+                "instead of member database id",
             )
         sub.add_argument(
             '-n','--number',
@@ -66,18 +51,24 @@ def parseArgumments():
             const='code',
             default='memberid',
             help="select members by its member code number, "
-                "instead member database id",
+                "instead of member database id",
             )
+    for sub in expire, assign:
+        sub.add_argument(
+            '-r','--ref',
+            dest='contractMode',
+            action='store_const',
+            const='ref',
+            default='id',
+            help="select contracts by its reference number, "
+                "instead of contract database id",
+            )
+    for sub in default,:
         """
         sub.add_argument(
             '--all',
             action='store_true',
             help='apply it to all members with investments',
-            )
-        sub.add_argument(
-            '--force',
-            action='store_true',
-            help='create even without ',
             )
         """
         sub.add_argument(
@@ -85,19 +76,30 @@ def parseArgumments():
             nargs='*',
             help='investors of Generation-kWh (see --partner and --number)',
             )
+    for sub in expire, assign,:
+        sub.add_argument(
+            'contract',
+            type=int,
+            help="contract database id (see also --ref)",
+            )
+        sub.add_argument(
+            'member',
+            type=int,
+            help="investor of Generation-kWh (see --partner and --number)",
+            )
+    for sub in assign,:
+        sub.add_argument(
+            'priority',
+            type=int,
+            help="a precedence number, ie. a contract with priority 2 has to wait "
+                " contracts with priority 1 to access new production.",
+            )
 
     return parser.parse_args(namespace=ns())
 
-def set(member=None,contract=None,priority=None):
-    expire(member=member,contract=contract)
-    c.GenerationkwhAssignment.create({
-        'member_id': member,
-        'contract_id': contract,
-        'priority': priority
-    })
-
 def preprocessMembers(members=None,idmode=None):
     """Turns members in which ever format to the ones required by commands"""
+
     if idmode=="partner":
         idmap = dict(c.GenerationkwhDealer.get_members_by_partners(members))
         return idmap.values()
@@ -108,19 +110,22 @@ def preprocessMembers(members=None,idmode=None):
 
     return members
 
+def preprocessContracts(contracts=None,contractMode=None):
+    """Turns contracts format into the ones required by commands"""
+
+    if contractMode=="ref":
+        idmap = dict(c.GenerationkwhDealer.get_contracts_by_ref(contracts))
+        return idmap.values()
+
+    return contracts
+
 
 def clear(**args):
     c.GenerationkwhAssignment.dropAll()
 
 def list(csv=False,**args):
     """
-        List active investments between start and stop, both included,
-        for the member of for any member if member is None.
-        If start is not specified, it lists activated before stop.
-        If stop is not specified, it list activated and not deactivated
-        before start.
-        If neither start or stop are specified all investments are listed
-        active or not.
+        List existing assignments.
     """
     def buildcsv(data):
         return u''.join((
@@ -157,7 +162,9 @@ def list(csv=False,**args):
 def expire(
         member=None,
         contract=None,
+        idmode=None,
         **_):
+    member = preprocessMembers([member], idmode)[0]
     step("Markin as expired assignation between contract {} and member {}"
         .format(contract, member))
     c.GenerationkwhAssignment.expire(contract, member)
@@ -173,6 +180,15 @@ def default(
         ','.join(str(i) for i in members)))
     c.GenerationkwhAssignment.createDefaultForMembers(members)
     success("Done.")
+
+def assign(member=None,contract=None,priority=None, idmode=None):
+    member = preprocessMembers([member], idmode)[0]
+    expire(member=member,contract=contract)
+    c.GenerationkwhAssignment.create({
+        'member_id': member,
+        'contract_id': contract,
+        'priority': priority
+    })
 
 
 c = erppeek.Client(**dbconfig.erppeek)
