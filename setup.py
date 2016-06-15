eaee269a48406932823ce18f13935bc921f3d1c0
David García Garzón <voki@canvoki.net>
2016-06-15 23:38:03 +0200

    version 1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ readme = open("README.rst").read()
 
 setup(
 	name = "plantmeter",
-	version = "1.1",
+	version = "1.2",
 	description =
 		"OpenERP module and library to manage multisite energy generation",
 	author = "Som Energia SCCL",
