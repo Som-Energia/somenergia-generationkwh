2c57b480109f1f8fe180f19ab5a067758e44a34e
David García Garzón <voki@canvoki.net>
2016-06-21 22:57:48 +0200

    version 1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ readme = open("README.md").read()
 
 setup(
 	name = "somenergia-generationkwh",
-	version = "1.0",
+	version = "1.2",
 	description =
 		"OpenERP module and library to manage Som Energia's Generation kWh",
 	author = "Som Energia SCCL",
