698d4a8cb6217a42c2206bdf83e39f32455560db
David García Garzón <voki@canvoki.net>
2016-09-22 13:56:57 +0200

    testing DHA with dst

--- generationkwh/fareperiodcurve_test.py
+++ generationkwh/fareperiodcurve_test.py
@@ -60,7 +60,6 @@ class FarePeriodCurve_Test(unittest.TestCase):
             )
         return p.periodMask(fare, period, isodate(start_date), isodate(end_date))
 
-        
     def assertArrayEqual(self, result, expected):
         result = list(result)
         result = [result[i:i+25] for i in xrange(0,len(result),25)]
@@ -72,8 +71,48 @@ class FarePeriodCurve_Test(unittest.TestCase):
 
         mask = self.setupCurve('2015-12-01', '2015-12-31', '2.0A', 'P1')
 
-        self.assertArrayEqual(mask, 
+        self.assertArrayEqual(mask,
             + 31 * ( [1]*24+[0] )
+            )
+
+    def test_20DHA_singleMonth_summer(self):
+        p = FarePeriodCurve(holidays=HolidaysProvidersMockup())
+
+        mask = self.setupCurve('2015-08-01', '2015-08-31', '2.0DHA', 'P2')
+
+        self.assertArrayEqual(mask,
+            + 31 * ( [1]*13+[0]*10+[1,0] )
+            )
+
+    def test_20DHA_singleMonth_winter(self):
+        p = FarePeriodCurve(holidays=HolidaysProvidersMockup())
+
+        mask = self.setupCurve('2015-12-01', '2015-12-31', '2.0DHA', 'P2')
+
+        self.assertArrayEqual(mask,
+            + 31 * ( [1]*12+[0]*10+[1,1,0] )
+            )
+
+    def test_20DHA_singleMonth_summerToWinter(self):
+        p = FarePeriodCurve(holidays=HolidaysProvidersMockup())
+
+        mask = self.setupCurve('2016-10-01', '2016-10-31', '2.0DHA', 'P2')
+
+        self.assertArrayEqual(mask,
+            + 29 * ( [1]*13+[0]*10+[1,0] )
+            +  1 * ( [1]*13+[0]*10+[1,1] )
+            +  1 * ( [1]*12+[0]*10+[1,1,0] )
+            )
+
+    def test_20DHA_singleMonth_winterToSummer(self):
+        p = FarePeriodCurve(holidays=HolidaysProvidersMockup())
+
+        mask = self.setupCurve('2016-03-01', '2016-03-31', '2.0DHA', 'P2')
+
+        self.assertArrayEqual(mask,
+            + 26 * ( [1]*12+[0]*10+[1,1,0] )
+            +  1 * ( [1]*12+[0]*10+[1,0,0] )
+            +  4 * ( [1]*13+[0]*10+[1,0] )
             )
 
     def test_31A_P1_singleMonth(self):
