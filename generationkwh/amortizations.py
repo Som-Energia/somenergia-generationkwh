c7673fc1c3f1801f64e10614e3ae5fa8138424f9
Alberto Rasillo <bluesnatural@gmail.com>
2017-05-10 16:49:49 +0200

    test_pendingAmortization_whenExpiredAndAllPending

--- generationkwh/amortizations.py
+++ generationkwh/amortizations.py
@@ -1,10 +1,19 @@
 # -*- coding:utf8 -*-
 
 def pendingAmortization(purchase_date, current_date, investment_amount, amortized_amount):
-    if purchase_date == current_date:
+    from plantmeter.isodates import isodate
+    from dateutil.relativedelta import relativedelta
+
+    current_date = isodate(current_date)
+    purchase_date = isodate(purchase_date)
+    years = relativedelta(current_date, purchase_date).years
+
+    if years < 2:
         return 0
 
-    return 400
+    if years >= 25:
+        return investment_amount
 
+    return (years-1)*400
 
 # vim: et ts=4 sw=4
--- generationkwh/amortizations_test.py
+++ generationkwh/amortizations_test.py
@@ -16,6 +16,15 @@ class Amortization_Test(unittest.TestCase):
             )
         self.assertEqual(a, 0)
 
+    def test_pendingAmortization_DayBeforeAnyAmortization(self):
+        a = pendingAmortization(
+            purchase_date='2001-01-01',
+            current_date='2002-12-31',
+            investment_amount=10000,
+            amortized_amount=0,
+            )
+        self.assertEqual(a, 0)
+
     def test_pendingAmortization_atFirstAmortization(self):
         a = pendingAmortization(
             purchase_date='2001-01-01',
@@ -24,6 +33,44 @@ class Amortization_Test(unittest.TestCase):
             amortized_amount=0,
             )
         self.assertEqual(a, 400)
+
+    def test_pendingAmortization_secondAmortitzationPending(self):
+        a = pendingAmortization(
+            purchase_date='2001-01-01',
+            current_date='2004-01-01',
+            investment_amount=10000,
+            amortized_amount=0,
+            )
+        self.assertEqual(a, 800)
+
+
+    def test_pendingAmortization_lastAmortitzationPending(self):
+        a = pendingAmortization(
+            purchase_date='2001-01-01',
+            current_date='2026-01-01',
+            investment_amount=10000,
+            amortized_amount=0,
+            )
+        self.assertEqual(a, 10000)
+
+    def test_pendingAmortization_whenExpiredAndAllPending(self):
+        a = pendingAmortization(
+            purchase_date='2001-01-01',
+            current_date='2028-01-01',
+            investment_amount=10000,
+            amortized_amount=0,
+            )
+        self.assertEqual(a, 10000)
+
+
+    def test_pendingAmortization_atFirstAmortizationAlreadyPaid(self):
+        a = pendingAmortization(
+            purchase_date='2001-01-01',
+            current_date='2003-01-01',
+            investment_amount=10000,
+            amortized_amount=400,
+            )
+        self.assertEqual(a, 0)
 
 
 
