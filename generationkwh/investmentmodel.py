060463464fc7f649f29dc08f34516a52ab5357cd
Francisco Cubero <Francisco.Cubero@somenergia.coop>
2017-07-31 14:32:15 +0200

    moved consts from investment to investmentmodel

--- generationkwh/investmentmodel.py
+++ generationkwh/investmentmodel.py
@@ -6,7 +6,9 @@ Parameters for the Generation kWh investment model
 
 shareValue = 100 # price in € for a sigle GkWh share
 
+# Must decide which constant survives in the next refactor
 waitYears = 1 # number of years until a Gkwh investment comes effective
+waitingDays = 365 # number of days until a Gkwh investment comes effective
 
 expirationYears = 25 # number of active years for a Gkwh investment 
 
