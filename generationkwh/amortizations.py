b7d4f43524adc8fb352568dd57da787a0a35d89d
David García Garzón <voki@canvoki.net>
2017-08-17 17:05:49 +0200

    splitted amortizations

--- generationkwh/amortizations.py
+++ generationkwh/amortizations.py
@@ -6,53 +6,39 @@ import generationkwh.investmentmodel as gkwh
 
 def previousAmortizationDate(purchase_date, current_date):
 
-    years = relativedelta(
-        isodate(current_date),
-        isodate(purchase_date),
-        ).years
-
-    if years <= gkwh.waitYears:
-        return None
-
-    firstAmortization = (
-        isodate(purchase_date)
-        + relativedelta(years = min(years,gkwh.expirationYears)
-        ))
-
-    return str(firstAmortization)
-
+    pending = pendingAmortizations(purchase_date, current_date, 100, 0)
+    if not pending: return None
+    return pending[-1][2]
 
 def pendingAmortization(purchase_date, current_date, investment_amount, amortized_amount):
 
-
-    years = relativedelta(
-        isodate(current_date),
-        isodate(purchase_date),
-        ).years
-
-    yearly_amortitzation = investment_amount / gkwh.expirationYears
-
-    if years <= gkwh.waitYears:
-        return 0
-
-    if years >= gkwh.expirationYears:
-        return investment_amount - amortized_amount
-
-    toAmortize = (years-1)*yearly_amortitzation - amortized_amount
-    return max(0, toAmortize)
+    pending = pendingAmortizations(purchase_date, current_date, investment_amount, amortized_amount)
+    if not pending: return 0
+    return sum(x[-1] for x in pending)
 
 def currentAmortizationNumber(purchase_date, current_date):
 
-    years = relativedelta(
-        isodate(current_date),
-        isodate(purchase_date),
-        ).years
-    if years < 2:
-        return None
-
-    return min(gkwh.expirationYears, years ) - 1
+    pending = pendingAmortizations(purchase_date, current_date, 100, 0)
+    if not pending: return None
+    return pending[-1][0]
 
 def totalAmortizationNumber():
     return gkwh.expirationYears - 1
+
+def pendingAmortizations(purchase_date, current_date, investment_amount, amortized_amount):
+    years = gkwh.expirationYears
+    yearlyAmount = investment_amount/years
+    return [
+        (i, years-1, amortization_date, (1 if i<years-1 else 2) * yearlyAmount)
+        for i, amortization_date in (
+            (i, str(isodate(purchase_date) + relativedelta(years=i+1)))
+            for i in xrange(1,years) 
+            )
+        if amortization_date <= current_date
+        and i * yearlyAmount > amortized_amount
+        ]
+
+
+
 
 # vim: et ts=4 sw=4
--- generationkwh/amortizations_test.py
+++ generationkwh/amortizations_test.py
@@ -4,12 +4,16 @@ import unittest
 
 from amortizations import (
     pendingAmortization,
+    pendingAmortizations,
     previousAmortizationDate,
     currentAmortizationNumber,
     totalAmortizationNumber,
     )
 
 class Amortization_Test(unittest.TestCase):
+
+    def setUp(self):
+        self.maxDiff = None
 
     def test_pendingAmortization_beforeAnyAmortization(self):
         a = pendingAmortization(
@@ -169,5 +173,105 @@ class Amortization_Test(unittest.TestCase):
 
     def test_totalAmortizationNumber_its_a_constant(self):
         self.assertEqual(totalAmortizationNumber(),24)
+
+    def test_pendingAmortizations_justFirstAmortization(self):
+        self.assertEqual(
+            pendingAmortizations(
+                purchase_date='2000-01-01',
+                current_date='2002-01-01',
+                investment_amount=1000,
+                amortized_amount=0,
+                ), [
+                (1, 24, '2002-01-01', 40),
+            ])
+
+    def test_pendingAmortizations_justBeforeFirstOne(self):
+        self.assertEqual(
+            pendingAmortizations(
+                purchase_date='2000-01-01',
+                current_date='2001-12-31',
+                investment_amount=1000,
+                amortized_amount=0,
+                ), [])
+
+    def test_pendingAmortizations_justSecondOne(self):
+        self.assertEqual(
+            pendingAmortizations(
+                purchase_date='2000-01-01',
+                current_date='2003-01-01',
+                investment_amount=1000,
+                amortized_amount=0,
+                ), [
+                (1, 24, '2002-01-01', 40),
+                (2, 24, '2003-01-01', 40),
+            ])
+
+    def test_pendingAmortizations_alreadyAmortized(self):
+        self.assertEqual(
+            pendingAmortizations(
+                purchase_date='2000-01-01',
+                current_date='2003-01-01',
+                investment_amount=1000,
+                amortized_amount=40,
+                ), [
+                (2, 24, '2003-01-01', 40),
+            ])
+
+    def test_pendingAmortizations_lastDouble(self):
+        self.assertEqual(
+            pendingAmortizations(
+                purchase_date='2000-01-01',
+                current_date='2025-01-01',
+                investment_amount=1000,
+                amortized_amount=920,
+                ), [
+                (24, 24, '2025-01-01', 80),
+            ])
+
+    def test_pendingAmortizations_allDone(self):
+        self.assertEqual(
+            pendingAmortizations(
+                purchase_date='2000-01-01',
+                current_date='2025-01-01',
+                investment_amount=1000,
+                amortized_amount=1000,
+                ), [
+            ])
+
+    def test_pendingAmortizations_allPending(self):
+        self.assertEqual(
+            pendingAmortizations(
+                purchase_date='2000-01-01',
+                current_date='2040-01-01',
+                investment_amount=1000,
+                amortized_amount=0,
+                ), [
+                ( 1, 24, '2002-01-01', 40),
+                ( 2, 24, '2003-01-01', 40),
+                ( 3, 24, '2004-01-01', 40),
+                ( 4, 24, '2005-01-01', 40),
+                ( 5, 24, '2006-01-01', 40),
+                ( 6, 24, '2007-01-01', 40),
+                ( 7, 24, '2008-01-01', 40),
+                ( 8, 24, '2009-01-01', 40),
+                ( 9, 24, '2010-01-01', 40),
+                (10, 24, '2011-01-01', 40),
+                (11, 24, '2012-01-01', 40),
+                (12, 24, '2013-01-01', 40),
+                (13, 24, '2014-01-01', 40),
+                (14, 24, '2015-01-01', 40),
+                (15, 24, '2016-01-01', 40),
+                (16, 24, '2017-01-01', 40),
+                (17, 24, '2018-01-01', 40),
+                (18, 24, '2019-01-01', 40),
+                (19, 24, '2020-01-01', 40),
+                (20, 24, '2021-01-01', 40),
+                (21, 24, '2022-01-01', 40),
+                (22, 24, '2023-01-01', 40),
+                (23, 24, '2024-01-01', 40),
+                (24, 24, '2025-01-01', 80),
+            ])
+
+
 
 # vim: et ts=4 sw=4
