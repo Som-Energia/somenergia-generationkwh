fad3ee33821fb69b992349cdef0d1c9289f1bb30
Alberto Rasillo <bluesnatural@gmail.com>
2017-08-17 17:52:41 +0200

    Poweremail Generation Impagament de la inversio

--- scripts/investment_test.py
+++ scripts/investment_test.py
@@ -1490,8 +1490,23 @@ class Investment_Test(unittest.TestCase):
         self.Investment.send_mail(id,'account.invoice', 'generationkwh_mail_pagament')
         self.assertNsEqual(self.MailMockup.log(), ns.loads("""\
             logs:
-                - PlantillaEmailEnviada: generationkwh_mail_creacio
-                - PlantillaEmailEnviada: generationkwh_mail_pagament
+                - generationkwh_mail_creacio
+                - generationkwh_mail_pagament
+           """))
+
+    def test__send_mail__emailImpagamentSent(self):
+        id = self.Investment.create_from_form(
+            self.personalData.partnerid,
+            '2017-01-01', # order_date
+            4000,
+            '10.10.23.123',
+            'ES7712341234161234567890',
+            )
+        self.Investment.send_mail(id,'account.invoice', 'generationkwh_mail_impagament')
+        self.assertNsEqual(self.MailMockup.log(), ns.loads("""\
+            logs:
+                - generationkwh_mail_creacio
+                - generationkwh_mail_impagament
            """))
 
     def test__amortized_amount__zeroByDefault(self):
