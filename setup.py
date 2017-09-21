d72a3d19d128ba4ce83890b3db06cdf80be0f20b
erp <erp@somenergia>
2017-09-21 17:54:29 +0200

    bump to 1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ readme = open("README.rst").read()
 
 setup(
 	name = "plantmeter",
-	version = "1.4",
+	version = "1.5",
 	description =
 		"OpenERP module and library to manage multisite energy generation",
 	author = "Som Energia SCCL",
