f82ed799e67d4aad3d37b5f7ed6fd998c5df6135
oriolpiera <oriol.piera@somenergia.coop>
2017-12-18 10:41:51 +0100

    Deploy 2.3 version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ readme = open("README.md").read()
 
 setup(
 	name = "somenergia-generationkwh",
-	version = "2.3-dev",
+	version = "2.3",
 	description =
 		"OpenERP module and library to manage Som Energia's Generation kWh",
 	author = "Som Energia SCCL",
