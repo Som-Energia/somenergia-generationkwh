580dbb9619fc7f98b6826995897ac35bd8471e95
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2018-04-03 13:04:51 +0200

    migrate genkwh_pull_status to python

---
+++ scripts/genkwh_pull_status.py
@@ -0,0 +1,15 @@
+import json
+from datetime import datetime, timedelta
+
+import erppeek
+import dbconfig
+
+O = erppeek.Client(**dbconfig.erppeek)
+
+date_2 = (datetime.now() - timedelta(days=2)).strftime('%Y-%m-%d')
+
+last_pulls_ids = O.GenerationkwhProductionNotifier.search([('date_pull', '>', date_2)])
+
+last_pulls = O.GenerationkwhProductionNotifier.read(last_pulls_ids)
+
+print(json.dumps(last_pulls, indent=4, sort_keys=True))
