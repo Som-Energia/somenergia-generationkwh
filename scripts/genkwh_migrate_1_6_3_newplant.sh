4c8d5616f4cc95c0c543b125241cd7e5ec51cdcc
David García Garzón <voki@canvoki.net>
2019-02-20 20:14:27 +0100

    genkwh_migrate_1_6_3_newplant: compact utils

--- scripts/genkwh_migrate_1_6_3_newplant.sh
+++ scripts/genkwh_migrate_1_6_3_newplant.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-function step() {
-    echo -e '\033[34;1m'"$*"'\033[0m'
-}
-function error() {
-    echo -e '\033[31;1m'"$*"'\033[0m'
-}
-function warn() {
-    echo -e '\033[33m'"$*"'\033[0m'
-}
-function fail() {
-    error "$*"
-    exit -1
-}
-function run() {
-    echo -e '\033[33;1m'"$*"'\033[0m'
-    "$@"
-}
+function step() { echo -e '\033[34;1m'"$*"'\033[0m'; }
+function error() { echo -e '\033[31;1m'"$*"'\033[0m'; }
+function warn() { echo -e '\033[33m'"$*"'\033[0m'; }
+function fail() { error "$*"; exit -1; }
+function run() { echo -e '\033[33;1m'"$*"'\033[0m'; "$@"; }
 
 ALCOLEA_COUNTER_SERIAL='501600324'
 FONTIVSOLAR_TOTAL_SHARES=20000 # TODO: review
@@ -25,6 +13,7 @@ FONTIVSOLAR_START_DATE='2019-02-20'
 
 step "Current state"
 run scripts/genkwh_production.py list
+
 step "Renaming Alcolea meter"
 run scripts/genkwh_production.py meterset \
     GenerationkWh Alcolea "1" name "$ALCOLEA_COUNTER_SERIAL"  ||
@@ -44,6 +33,7 @@ run scripts/genkwh_production.py addmeter \
 step "Enabling new plant"
 run scripts/genkwh_production.py plantset GenerationkWh Fontivsolar enabled '1' ||
     fail "Unable to enable the new plant"
+
 step "Enabling new meter"
 run scripts/genkwh_production.py meterset GenerationkWh Fontivsolar 68308479 enabled '1' ||
     fail "Unable to enable the new meter"
