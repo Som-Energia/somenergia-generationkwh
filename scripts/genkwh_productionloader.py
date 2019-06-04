ff0add99b0de21590a4f92c548544adaf4377d60
David García Garzón <voki@canvoki.net>
2019-06-04 09:08:32 +0200

    genkwh_productionloader: output some info

--- scripts/genkwh_productionloader.py
+++ scripts/genkwh_productionloader.py
@@ -29,8 +29,16 @@ def parseArgumments():
     return parser.parse_args(namespace=ns())
 
 def computerights(id=None):
+    Remainder = c.GenerationkwhRemainderTesthelper
+    step("Next day to compute (nshares, date):")
+    for nshares, day, remainderWh in sorted(Remainder.lastRemainders()):
+        print nshares, day
+    step("Computing production rights for each number of shares...")
     productionloader_obj = c.GenerationkwhProductionLoader
     productionloader_obj.computeAvailableRights(id)
+    step("Next day to compute after update...")
+    for nshares, day, remainderWh in sorted(Remainder.lastRemainders()):
+        print nshares, day
 
 c = erppeek.Client(**dbconfig.erppeek)
 
