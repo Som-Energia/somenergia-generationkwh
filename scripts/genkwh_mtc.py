26c628872e45312e63ea58ab70ba9ed7b87cd2d0
David García Garzón <voki@canvoki.net>
2019-07-23 19:04:21 +0200

    --by display aggregation option, extracted mtc

--- scripts/genkwh_mtc.py
+++ scripts/genkwh_mtc.py
@@ -63,23 +63,9 @@ sources = ns.loads("""
         creationfield: create_at
 """)
 
-@mtc.command()
-@click.argument('type', type=click.Choice(sources.keys()))
-@click.argument('name', required=False)
-@click.option('--output', '-o')
-@click.option('--database', '-d', default='somenergia')
-@click.option('--from','-f', type=localisodate, default="2016-05-01")
-@click.option('--to','-t', type=localisodate, default=str(datetime.date.today()))
-def curve(database, type, name, **args):
-    """
-    Outputs in a tabular format a mongo time curve
 
-    $ scripts/genkwh_production.py curve gisce 501600324
-
-    $ scripts/genkwh_production.py curve production 1
-    """
-
-    source = sources[type]
+def getMongoTimeCurve(database, curveName, filter, firstDate, lastDate):
+    source = sources[curveName]
     c = pymongo.MongoClient(**dbconfig.mongo)
     mongodb = c[database]
     mtc = MongoTimeCurve(
@@ -88,19 +74,15 @@ def curve(database, type, name, **args):
         source.timefield,
         source.creationfield,
         )
-    curve = mtc.get(
-        start=args.get('from',None),
-        stop=args.get('to',None),
-        filter=None if name is None else long(name) if source.get('intname',False) else name,
+    if source.get('intname',False):
+        filter = filter and long(filter)
+
+    return mtc.get(
+        start=firstDate,
+        stop=lastDate,
+        filter=filter,
         field=source.datafield,
         )
-    result = displayDayHourMatrix(args['from'], curve )
-    output = args.get('output',None)
-    if output:
-        with io.open(output) as output_file:
-            output_file.write(result)
-    else:
-        print result
 
 def displayDayHourMatrix(firstDate, curve):
     import numpy
@@ -117,6 +99,96 @@ def displayDayHourMatrix(firstDate, curve):
         in enumerate(numpy.reshape(curve,(-1,25)))
     ) + "\nTotal {}".format(sum(curve))
 
+
+def displayMonthMatrix(firstDate, curve):
+    import numpy
+    daily = numpy.sum(numpy.reshape(curve, (-1, 25)), axis=1)
+    dates = [ addDays(firstDate, i).date() for i in range(len(daily)) ]
+    monthfirst = [
+        i
+        for i, (date, yesterday)
+        in enumerate(zip(dates, [None]+dates))
+        if yesterday is None
+        or date.month != yesterday.month
+    ]
+    monthly = numpy.array([
+        numpy.sum(month)
+        for month
+        in numpy.split(daily, monthfirst)
+        ])
+    return '\n'.join(
+        '{:%Y-%m} {}'.format(
+            addDays(firstDate, idate),
+            value,
+        )
+        for idate, value in zip(monthfirst, monthly)
+        )+ "\nTotal {}\n".format(monthly.sum())
+
+def displayMonthHourMatrix(firstDate, curve):
+    import numpy
+    daily = numpy.reshape(curve, (-1, 25))
+    dates = [ addDays(firstDate, i).date() for i in range(len(daily)) ]
+    monthfirst = [
+        i
+        for i, (date, yesterday)
+        in enumerate(zip(dates, [None]+dates))
+        if yesterday is None
+        or date.month != yesterday.month
+    ]
+    monthly = numpy.array([
+        month.sum(axis=0)
+        for month
+        in numpy.split(daily, monthfirst)
+        ])
+    return '\n'.join(
+        '{:%Y-%m}{} {: 7d}'.format(
+            addDays(firstDate, idate),
+            ' '.join(format(x,' 7d') for x in value),
+            value.sum(),
+        )
+        for idate, value in zip(monthfirst, monthly)
+        )+ "\nTotal {}\n".format(monthly.sum())
+
+
+
+@mtc.command()
+@click.argument('type', type=click.Choice(sources.keys()))
+@click.argument('name', required=False)
+@click.option('--output', '-o')
+@click.option('--database', '-d', default='somenergia')
+@click.option('--from','-f', type=localisodate, default="2016-05-01")
+@click.option('--to','-t', type=localisodate, default=str(datetime.date.today()))
+@click.option('--by', type=click.Choice([
+    'dayhour',
+    'monthhour',
+    'day',
+    'month',
+    ]),
+    default='dayhour') 
+def curve(database, type, name, **args):
+    """
+    Outputs in a tabular format a mongo time curve
+
+    $ scripts/genkwh_production.py curve gisce 501600324
+
+    $ scripts/genkwh_production.py curve production 1
+    """
+    firstDate = args.get('from', None)
+    lastDate = args.get('to',None)
+    curve = getMongoTimeCurve(database, type, name, firstDate, lastDate)
+
+    display = dict(
+        dayhour = displayDayHourMatrix,
+        monthhour = displayMonthHourMatrix,
+        month = displayMonthMatrix,
+    )
+    result = display[args.get('by', 'dayhour')](firstDate, curve)
+    output = args.get('output',None)
+    if output:
+        with io.open(output) as output_file:
+            output_file.write(result)
+    else:
+        print result
 
 
 
