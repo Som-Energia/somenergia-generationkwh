19e039e3b96069f919bef6e5d9910495b6efb932
Oriol Piera <oriol.piera@somenergia.coop>
2019-08-06 10:30:11 +0200

    Add field is_gkwh is contract for module som_infoenergia

--- som_generationkwh/giscedata_polissa.py
+++ som_generationkwh/giscedata_polissa.py
@@ -16,3 +16,45 @@ class GiscedataPolissaTarifaPeriodes(osv.osv):
     }
 
 GiscedataPolissaTarifaPeriodes()
+
+class GiscedataPolissa(osv.osv):
+
+   _name = 'giscedata.polissa'
+   _inherit = 'giscedata.polissa'
+
+
+   def _search_is_gkwh(self, cursor, uid, obj, name, args, context=None):
+       """Search function for is_gkwh"""
+       cursor.execute(
+           'SELECT distinct contract_id FROM generationkwh_assignment'
+       )
+
+       gkwh_ids = [t[0] for t in cursor.fetchall()]
+       return [('id', 'in', gkwh_ids)]
+
+   def _ff_is_gkwh(self, cursor, uid, ids, field_name, arg, context=None):
+       """Returns true if contract has gkwh lines"""
+       if not ids:
+           return []
+       res = dict([(i, False) for i in ids])
+
+       vals = self.read(cursor, uid, ids, ['gkwh_linia_ids'])
+       for val in vals:
+           res.update({val['id']: bool(val['gkwh_linia_ids'])})
+
+       return res
+
+   _columns = {
+       'gkwh_linia_ids': fields.one2many(
+           'generationkwh.assignment', 'contract_id',
+           'Contractes benefeciats per Generation kWH', readonly=True
+       ),
+       'is_gkwh': fields.function(
+           _ff_is_gkwh,
+           method=True,
+           string='Te Generation', type='boolean',
+           fnct_search=_search_is_gkwh
+       )
+   }
+
+GiscedataPolissa()
