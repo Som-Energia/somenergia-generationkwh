4bafaf2a6258b3415c823880d9d0669f6d4a8cb9
Alberto <bluesnatural@gmail.com>
2020-02-11 12:11:07 +0100

    Refactor to scriptlauncher

--- scripts/integral_batch_file.py
+++ scripts/integral_batch_file.py
@@ -6,28 +6,23 @@ from scipy import integrate
 import pandas as pd
 from datetime import datetime
 
-def show_usage():
-    print('Usage: python integral_batch_file.py <input_csv_file> <output_csv_file>')
-
-def trapezoidal_approximation(ordered_sensors, day, outputDataFormat='%d/%m/%Y %H:%M:%S', timeSpacing=5./60.):
+def trapezoidal_approximation(ordered_sensors, from_date, to_date, outputDataFormat='%d/%m/%Y %H:%M:%S', timeSpacing=5./60., column2Integrate=' Irradiation'):
     ''' Trapezoidal aproximation of the 24 hours following first sensor entry'''
 
     '''Get the first date entry and create hourly time entries'''
-    ts_start = pd.Series(pd.date_range(start=day, periods=24, freq='H'))
+    ts_start = pd.Series(pd.date_range(start=from_date, end=to_date, freq='H'))
 
     deltaT = pd.Timedelta(hours=1)
     timeStrings = ts_start.dt.strftime(outputDataFormat)
-
     integrals = []
     for start, timeString in zip(ts_start, timeStrings):
         end = start + deltaT
         interval4integral = ordered_sensors.loc[start:end]
 
         ''' Trapezoidal aproximation of the hour following first sensor entry '''
-        test_integral = integrate.trapz(y = interval4integral[' Irradiation'], dx = timeSpacing)
+        test_integral = integrate.trapz(y = interval4integral[column2Integrate], dx = timeSpacing)
 
         integrals.append((timeString, test_integral))
-
     return integrals
 
 def asciigraph_print(tuple_array, scale_factor = 10):
@@ -36,55 +31,40 @@ def asciigraph_print(tuple_array, scale_factor = 10):
     for t, v in tuple_array:
         print("{} {}".format(t, '=' * int(v/scale_factor)))
 
-def plot_dataframe(dataFrame, output_png = 'graph.png'):
-    ''' WIP plotting the irradiation daily graph'''
-
-    dataFrame.plot()
-    # import matplotlib as plt
-    #
-    # plt.plot(tuple_array)
-    # plt.savefig(output_png)
-    # plt.show()
-
 def main():
 
-    if len(sys.argv[1:]) != 2:
-        print('Expecting 2 arguments, got {}'.format(len(sys.argv[1:])))
-        show_usage()
+    if len(sys.argv[1:]) != 3:
+        print('Expecting 3 arguments, got {}'.format(len(sys.argv[1:])))
         sys.exit(-1)
 
     '''TODO: use config file'''
     outputDataFormat = '%d/%m/%Y %H:%M:%S'
 
     '''TODO: sanitize'''
-    input_csv_file = sys.argv[1]
-    output_csv_file = sys.argv[2]
+    column2Integrate = sys.argv[1]
+    input_csv_file = sys.argv[2]
+    output_csv_file = sys.argv[3]
 
 
     sensors = pd.read_csv(input_csv_file, delimiter=';', encoding='utf-8')
     sensors['date_'] = sensors['DATE'] + sensors[' TIME']
-    sensors['date_'] = pd.to_datetime(sensors['date_'])
+    sensors['date_'] = pd.to_datetime(sensors['date_'], format='%d/%m/%Y %H:%M')
 
     ordered_sensors = sensors.set_index('date_')
 
     ''' Time in hours between readings '''
     timeSpacing = 5./60.
 
-    dayToProcess = ordered_sensors['DATE'][0]
+    from_date = pd.to_datetime(ordered_sensors['DATE'][0], format='%d/%m/%Y')
+    to_date = pd.to_datetime(ordered_sensors['DATE'][-1], format='%d/%m/%Y')
 
-    integrals = trapezoidal_approximation(ordered_sensors, dayToProcess, outputDataFormat, timeSpacing)
+    integrals = trapezoidal_approximation(ordered_sensors, from_date, to_date, outputDataFormat, timeSpacing, column2Integrate)
 
-    integralsDF = pd.DataFrame(data = integrals, columns = ['datetime', 'Irradiation'])
+    integralsDF = pd.DataFrame(data = integrals, columns = ['datetime', column2Integrate])
     integralsDF.to_csv(output_csv_file, sep=';', encoding='utf-8', index=False)
 
-    #print('\n'.join('{}: {}'.format(*k) for k in integrals))
-
-    #asciigraph_print(integrals, 10)
-
-    plot_dataframe(integralsDF)
-
-    print("Saved {} records".format(len(integralsDF)))
+    print("Saved {} records from {} to {}".format(len(integralsDF), from_date, to_date))
     print("Job's done, have a good day")
-
+    asciigraph_print(integrals)
 if __name__ == "__main__":
 	main()
