fa956f3b467e24003d7d8f3425642986e4b8406b
Mar Jene <mar.jene@somenergia.coop>
2022-09-28 15:37:07 +0200

    fix text

--- scripts/genkwh_rights_janitor.py
+++ scripts/genkwh_rights_janitor.py
@@ -150,7 +150,7 @@ def get_incoherent_rights(member_id):
             #cal tenir en compte que si no hi ha diferències entre el dia D i avui, vol dir que tot ok.
 
     if accions:
-        print("El partner {} ha desquadrat per un total de {} drets (mongo - erp).".format(str(member_id), str(total)))
+        print("El member {} ha desquadrat per un total de {} drets (mongo - erp).".format(str(member_id), str(total)))
     return accions
 
 def insert_actions_mongo(actions_filename):
